f36f3388fe812d78582c1e218496d54588d0ac93
Robson Magno <robsonmagnosistemas@gmail.com>
2019-03-04 18:11:05 -0400

    Inicio do projeto de estudos em Android.

---
+++ app/src/main/java/com/robsonmagno/bollyfilmes/views/MainActivity.java
@@ -0,0 +1,15 @@
+package com.robsonmagno.bollyfilmes.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.robsonmagno.bollyfilmes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
